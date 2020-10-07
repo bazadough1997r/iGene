6a4a43fd768d0df9e2a3857ab8e9f6df86ba4af2
Rawan Bazadough <bazadough1997@gmail.com>
2020-10-07 15:52:40 +0300

    start functioning

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("availability").addEventListener("click", available);
+
+function available() {
+	alert("Simulations are not available right now!");
+}
+
+// show rigester page
+document.getElementById("yourprofile").addEventListener("click", registerform);
+
+function registerform(){
+	console.log("account");
+document.getElementById("register").style.visibility = "visible";
+   console.log("account2");
+
+}
